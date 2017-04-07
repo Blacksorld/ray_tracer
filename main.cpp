e54543ca6183baf4d6672ff12a957b911616d07c
randan <alagaster@yandex.ru>
2017-04-07 05:53:50 +0300

    Add sample.

--- main.cpp
+++ main.cpp
@@ -2,34 +2,10 @@
 #include <iostream>
 
 
-void draw2() {
-    vector3d observer(0, 0, 0);
-
-    screen screen1(vector3d(-10, 10, 10), vector3d(1, 0, 0), vector3d(0, 0, -1), 20, 20);
-
-    std::vector<object*> objects(2);
-    objects[0] = new sphere(sf::Color(0, 0, 255), 0, 0, 0, vector3d(0, 20, 0), 5);
-    objects[1] = new quadrilateral(sf::Color::White, 0, 0, 0.5, vector3d(10, -30, -30), vector3d(10, -30, 30),
-                                   vector3d(10, 30, 30),
-                                   vector3d(10, 30, -30));
-
-    std::vector<illuminant> il;
-    il.push_back(illuminant(vector3d(-10, 0, 0), 350));
-
-
-    ray_tracer tracer( objects, il);
-
-    tracer.render(screen1, observer, 800, 800, 1);
-    tracer.draw();
-
-    delete objects[0];
-    delete objects[1];
-}
-
 int main()
 {
     std::ios_base::sync_with_stdio(false);
-    rt_parser parser("../samples/sample3.rt");
+    rt_parser parser("../samples/sample4.rt");
     parser.parse();
     parser.render();
     return 0;
